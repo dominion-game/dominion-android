cb60014eca2f2cb30717d8b4cc2aa4c45dcbc5d4
edubois9119 <erica.dubois9119@gmail.com>
2019-11-26 16:56:39 -0700

    merged ViewPager in UI branch. All is working as expected! fixed toolbar and options menu

--- app/src/main/java/edu/cnm/deepdive/dominionandroid/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/dominionandroid/controller/MainActivity.java
@@ -4,10 +4,10 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toolbar;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 import edu.cnm.deepdive.dominionandroid.R;
 import edu.cnm.deepdive.dominionandroid.service.GoogleSignInService;
@@ -20,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-//    Toolbar toolbar = findViewById(R.id.toolbar);
-//    setSupportActionBar(toolbar);
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
   }
 
   @Override
